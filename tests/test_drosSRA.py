297d8a467ea76de9374bfedaa5eb81ad34358f78
Isabelle <isabelle@gwu.edu>
2018-10-22 08:50:57 -0400

    add test case with multiple flags

--- tests/test_drosSRA.py
+++ tests/test_drosSRA.py
@@ -15,8 +15,13 @@ def response():
     """
     return requests.get('https://github.com/isabelleberger/drosSRA')
 
-def test_drosSRA(pubmed_id, capsys):
+def test_single_flag(pubmed_id, capsys):
     #drosSRA.main(pubmed_id)
     test_run = drosSRA.main(['-p','24151578.0'])
     out, err = capsys.readouterr()
     assert out=='https://www.ncbi.nlm.nih.gov/genome/gdv/browser/?context=GEO&acc=DRX013093%2CDRX013094\n'
+
+def test_mult_flags(pubmed_id, capsys):
+    test_run = drosSRA.main(['-p', '27172187.0', '-s','female'])
+    out, err = capsys.readouterr()
+    assert out=='https://www.ncbi.nlm.nih.gov/genome/gdv/browser/?context=GEO&acc=SRX357301%2CSRX357302%2CSRX357303%2CSRX357318%2CSRX357319%2CSRX357320%2CSRX357324%2CSRX357325%2CSRX357326%2CSRX357330%2CSRX357331%2CSRX357332%2CSRX357336%2CSRX357337%2CSRX357338%2CSRX971847%2CSRX971848%2CSRX971849%2CSRX971850%2CSRX971851%2CSRX971852\n'
